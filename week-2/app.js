251c99d78bac69f181e9478a95f35456d3a0bc0c
Ralph Hung <80564528+juifuhung@users.noreply.github.com>
2022-04-01 12:02:53 +0800

    Call To Action button can load more content boxes

--- week-2/app.js
+++ week-2/app.js
@@ -7,8 +7,55 @@ welcome.addEventListener("click", () => {
     }
 })
 
-let loadMore = document.querySelector("main section.button button");
-let moreContents = document.querySelector("main section.more")
-loadMore.addEventListener("click", () => {
-    moreContents.style.display = "flex";
+let CallToAction = document.querySelector("section.button button");
+let moreContents = document.querySelector("section.more");
+CallToAction.addEventListener("click", () => {
+
+    let newContentBox = document.createElement("section");
+    newContentBox.classList.add("new-content-box");
+    
+    let newUpper = document.createElement("section");
+    newUpper.classList.add("upper");
+
+    let newTopLeft = document.createElement("div");
+    newTopLeft.classList.add("top-left");
+
+    let newTopLeftContent = document.createElement("h4");
+    newTopLeftContent.innerText="Content Box 1";
+    newTopLeft.appendChild(newTopLeftContent);
+
+    let newTopRight = document.createElement("div");
+    newTopRight.classList.add("top-right");
+
+    let newTopRightContent = document.createElement("h4");
+    newTopRightContent.innerText="Content Box 2";
+    newTopRight.appendChild(newTopRightContent);
+
+    newUpper.appendChild(newTopLeft);
+    newUpper.appendChild(newTopRight);
+
+    let newLower = document.createElement("section");
+    newLower.classList.add("lower");
+
+    let newLowerLeft = document.createElement("div");
+    newLowerLeft.classList.add("bottom-left");
+
+    let newLowerLeftContent = document.createElement("h4");
+    newLowerLeftContent.innerText="Content Box 3";
+    newLowerLeft.appendChild(newLowerLeftContent);
+
+    let newLowerRight = document.createElement("div");
+    newLowerRight.classList.add("bottom-right");
+
+    let newLowerRightContent = document.createElement("h4");
+    newLowerRightContent.innerText="Content Box 4";
+    newLowerRight.appendChild(newLowerRightContent);
+
+    newLower.appendChild(newLowerLeft);
+    newLower.appendChild(newLowerRight);
+
+    newContentBox.appendChild(newUpper);
+    newContentBox.appendChild(newLower);
+
+    moreContents.appendChild(newContentBox);
 })
