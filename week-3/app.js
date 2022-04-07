1003e7ed83b6867d0b67cbb69c6574d3981e8c11
Ralph Hung <80564528+juifuhung@users.noreply.github.com>
2022-04-07 14:04:51 +0800

    redirection completed

--- week-3/app.js
+++ week-3/app.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+
 app.set('view engine', 'pug');
+
+app.get('/', (req,res) => {
+    const name = req.cookies.username;
+    if(name){
+        res.render('index', {name}); //原{name:name}，用ES6 shorthand
+    } else {
+        res.redirect('/hello');
+    }
+});
 
 app.get('/hello', (req,res) => {
     res.render('hello');
 });
 
+app.get('/hello', (req,res) => {
+    const name = req.cookies.username;
+    if(name){
+        res.redirect('/'); 
+    } else {
+        res.render('/hello');
+    }
+});
+
 app.post('/hello', (req,res) => {
-    res.render('hello', {name: req.body.username});
+    res.cookie('username', req.body.username); //send cookie to browser
+    res.redirect('/');
+});
+
+app.post('/goodbye', (req,res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
 });
 
 app.listen(3000, () => {
