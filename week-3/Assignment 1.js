19b4581f36c596f897fc59f21422a4dcbe6d8dcf
Ralph Hung <80564528+juifuhung@users.noreply.github.com>
2022-04-08 16:21:24 +0800

    changed to express.js codes

--- week-3/Assignment 1.js
+++ week-3/Assignment 1.js
@@ -1,9 +1,11 @@
-doctype html
-html(lang="en")
-    head
-        meta(charset="UTF-8")
-        meta(http-equiv="X-UA-Compatible", content="IE=edge")
-        meta(name="viewport", content="width=device-width, initial-scale=1.0")
-        title Hello, My Server!
-    body
-        h1 Hello, My Server!
+const express = require("express");
+const app = express();
+app.set('view engine', 'pug');
+
+app.get('/', (req,res) => {
+    res.render('index');  
+});
+
+app.listen(3000, () => {
+    console.log("Server listening on Port 3000.");
+});
