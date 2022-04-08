5fdb4d506461f70db87af86b2779eb238b343667
Ralph Hung <80564528+juifuhung@users.noreply.github.com>
2022-04-08 08:43:44 +0800

    get can store cookies but can't redirect to myName

--- week-3/app.js
+++ week-3/app.js
@@ -1,45 +1,42 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
-
+ 
 app.set('view engine', 'pug');
 
-app.get('/', (req,res) => {
-    const name = req.cookies.username;
-    if(name){
-        res.render('index', {name}); //原{name:name}，用ES6 shorthand
-    } else {
-        res.redirect('/hello');
-    }
+app.get('/trackName', (req,res) => {
+    res.cookie('name', req.query.name);
+    res.render('trackName');
 });
 
-app.get('/hello', (req,res) => {
-    res.render('hello');
+app.get('/trackName', (req,res) => {
+    res.redirect('myName');
 });
 
-app.get('/hello', (req,res) => {
-    const name = req.cookies.username;
-    if(name){
-        res.redirect('/'); 
-    } else {
-        res.render('/hello');
-    }
+app.get('/myName', (req,res) => {
+    res.render('myName', {namme: req.cookies.name});
 });
 
-app.post('/hello', (req,res) => {
-    res.cookie('username', req.body.username); //send cookie to browser
-    res.redirect('/');
-});
 
-app.post('/goodbye', (req,res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
+//trackName用post ok
+// app.get('/myName', (req,res) => {
+//     res.render('myName', {namme: req.cookies.name});
+// });
 
+// app.get('/trackName', (req,res) => {
+//     res.render('trackName');
+// });
+
+// app.post('/trackName', (req,res) => {
+//     res.cookie('name', req.body.name); 
+//     res.redirect('myName');
+// });
+//trackName用post ok
+ 
 app.listen(3000, () => {
-    console.log("Server running on localhost:3000");
+    console.log("Server listening on port 3000.");
 });
