1b09393ed8af6fb629898dceeab301902247f2c4
Ralph Hung <80564528+juifuhung@users.noreply.github.com>
2022-04-18 11:19:30 +0800

    Update Assignment 1.js

--- week-4/Assignment 1.js
+++ week-4/Assignment 1.js
@@ -1,7 +1,8 @@
-function delayedResult(n1,n2,delayTime, callback){       
-    setTimeout(() => {
-        callback(n1+n2 + " (" + n1 + "+" + n2 + ")");
-    }, delayTime);
+function delayedResult(n1, n2, delayTime, callback) {
+  setTimeout(() => {
+    callback(n1 + n2 + " (" + n1 + "+" + n2 + ")");
+  }, delayTime);
 }
 
-delayedResult(6,9,3000, (result) => console.log(result));
+delayedResult(6, 9, 3000, function(result) {console.log(result)});
+delayedResult(-5, 10, 2000, function(result) {window.alert(result)});
